7e98bcad71fe180acdf37ce9d633e726539c0078
Istvan Fehervari <gooksl@gmail.com>
2017-05-22 20:43:12 +0200

    Created an eclipse project

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+/*
+ * This file is part of the UnityPack rust package.
+ * (c) Istvan Fehervari <gooksl@gmail.com>
+ *
+ * All rights reserved
+ */
+
+fn main() {
+    println!("Hello, world2!");
+}
